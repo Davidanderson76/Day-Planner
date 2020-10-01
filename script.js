198d1396044a60948642f9d8d3dd76cc102ce27b
David Anderson <david.anderson1993@gmail.com>
2020-10-01 00:42:44 -0500

    updating files before calling it a night

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-//updating clock/calander
+// Self updating clock/calander
 var currentTime = moment().format("dddd, MMMM Do"),
     HH = moment().format("HH");
 
 $("#currentDay").html(currentTime);
 
+//creating variables 
 
+
+
+//making a loop for the time to display throughout the day
+//for loop?
+
+//loop referencing the colock that is displayed on page
+
+//loop also pulls any saved items 
+
+for (let i = 9; i < 18; i++) {
+    var timeBlocks = document.getElementById("time-block");
+    var pullItems = "save" + i + "t";
+    if (i < HH) {
+        timeBlocks.classList.add("past");
+    }
+    if (i === HH) {
+        timeBlocks.classList.add("present");
+    }
+    if (i > HH) {
+        timeBlocks.classList.add("future");
+    }
+    document.getElementById(pullItems).value = localStorage.getItem(pullItems); 
+    //pulls any saved info from local storage and displays it accordingly
+}
 
 
 
@@ -12,11 +37,6 @@ $("#currentDay").html(currentTime);
 //LOCAL STORAGE ROUGH IDEA
 
 //pull over the textarea element
-$("i").click(function()  {
-    var saving = $(this).attr("id") + "t";
-    var saveInfo = document.getElementById(saving).value
-    localStorage.setItem(saving, saveInfo);
-});
 
 
 
