1c70bfb0f60f83d2d98d36cce53839d283aca34a
David Anderson <david.anderson1993@gmail.com>
2020-09-30 21:29:20 -0500

    upadting my files and ading the js script

---
+++ script.js
@@ -0,0 +1,7 @@
+var currentDay = document.getElementById("#currentDay");
+
+
+
+
+//LOCAL STORAGE ROUGH IDEA
+//document.getElementById("MyScore").innerHTML = "Your Final Score is: " + finalCountdown.toString();
