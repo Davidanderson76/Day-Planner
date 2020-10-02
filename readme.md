d705067d78827bd34e53763ae1d2c4d99c3c911d
David Anderson <david.anderson1993@gmail.com>
2020-10-01 23:43:28 -0500

    updating my j script and adding a read me and screenshot of the application

---
+++ readme.md
@@ -0,0 +1,10 @@
+Simple Work Day planner
+
+Here I have built a simple yet aesthetically pleasing planner application.
+The user is able to input tasks into the time blocks to assist them with planning day to day activities.
+The date displayed up top will update with the current date.
+
+Functions to be added include:
+updating the planner to show what time of day it is with color coding of time boxes.
+A save and clear function to save tasks to local storage and a clear all button if the user did not want to refresh the page
+after saving blank time slots.
--- script.js
+++ script.js
@@ -4,7 +4,41 @@ var currentTime = moment().format("dddd, MMMM Do"),
 
 $("#currentDay").html(currentTime);
 
-//creating variables 
+//creating the function to save to local storage
+//
+
+
+$("#fa-save").onclick(function() {
+    var textArea = document.getElementsByClassName(".time-block")
+        text = document.getElementById(textarea).value;
+    localStorage.setItem(textArea, text);
+});
+
+
+
+
+
+
+// pulling from local storage
+// let savedPlans = JSON.parse(localStorage.getItem("savedPlans"));
+// localStorage.setItem("storedPlans", JSON.stringify(planTextArray));
+
+
+// if (savedPlans !== null) {
+//     planTextArray = storedPlans;
+// }
+
+// var textDiv = $(".savebutton");
+
+// var texts = document.querySelector(".time-block");
+
+// var plans = localStorage.getItem("count");
+
+// textDiv.on("click", function(){
+//     texts.textContent = plans;
+
+//     localStorage.setItem("count", plans);
+// })
 
 
 
@@ -15,21 +49,20 @@ $("#currentDay").html(currentTime);
 
 //loop also pulls any saved items 
 
-for (let i = 9; i < 18; i++) {
-    var timeBlocks = document.getElementById("time-block");
-    var pullItems = "save" + i + "t";
-    if (i < HH) {
-        timeBlocks.classList.add("past");
-    }
-    if (i === HH) {
-        timeBlocks.classList.add("present");
-    }
-    if (i > HH) {
-        timeBlocks.classList.add("future");
-    }
-    document.getElementById(pullItems).value = localStorage.getItem(pullItems); 
+// for (let i = 9; i < 18; i++) {
+//     var timeBlocks = document.getElementById("time-block");
+//     var pullItems = "save" + i + "t";
+//     if (i < HH) {
+//         timeBlocks.classList.add("past");
+//     }
+//     if (i === HH) {
+//         timeBlocks.classList.add("present");
+//     }
+//     if (i > HH) {
+//         timeBlocks.classList.add("future");
+//     }
+//     document.getElementById(pullItems).value = localStorage.getItem(pullItems); 
     //pulls any saved info from local storage and displays it accordingly
-}
 
 
 
